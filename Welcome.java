88b21d57a711e2f1248d323232cbde2d396d967b
minseo-00 <limminsoe08@gmail.com>
2025-05-26 17:43:54 +0900

    feat: 메뉴 반복 및 종료 기능(while, quit 변수) 추가

--- Welcome.java
+++ Welcome.java
@@ -4,30 +4,20 @@ import java.util.Scanner;
 
 public class Welcome {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-
-        System.out.print("당신의 이름을 입력하세요 : ");
-        String userName = input.next();
-
-        System.out.print("연락처를 입력하세요 : ");
-        int userMobile = input.nextInt();
-
-        String greeting = "Welcome to Shopping Mall";
-        String tagline = "Welcome to Book Market!";
-        
-        System.out.println("***************************************");
-        System.out.println("\t"+greeting);
-        System.out.println("\t"+tagline);
-        System.out.println("***************************************");
-        System.out.println("1. 고객 정보 확인하기 \t4. 바구니에 항목 추가하기");
-        System.out.println("2. 장바구니 상품 목록 보기 \t5. 장바구니의 항목 수량 줄이기");
-        System.out.println("3. 장바구니 비우기 \t6. 장바구니의 항목 삭제하기");
-        System.out.println("7. 영수증 표시하기 \t8. 종료");
-        System.out.println("***************************************");
-        System.out.println("메뉴 번호를 선택해주세요 ");
-        int n = input.nextInt(); // 메뉴 번호 입력
-        System.out.println(n +"번을 선택했습니다. "); // 입력한 번호 출력
+		boolean quit = false; // 종료 여부 설정 변수
 		
-	}
+		while(!quit) { // quit 변수가 true일 때까지 계속 반복
+		System.out.println("***************************************");
+		System.out.println("\t"+greeting);
+		System.out.println("\t"+tagline);
+		System.out.println("***************************************");
+		System.out.println("1. 고객 정보 확인하기 \t4. 바구니에 항목 추가하기");
+		System.out.println("2. 장바구니 상품 목록 보기 \t5. 장바구니의 항목 수량 줄이기");
+		System.out.println("3. 장바구니 비우기 \t6. 장바구니의 항목 삭제하기");
+		System.out.println("7. 영수증 표시하기 \t8. 종료");
+		System.out.println("***************************************");
+		System.out.println("메뉴 번호를 선택해주세요 ");
+		int n = input.nextInt(); 
+		
 }
 
