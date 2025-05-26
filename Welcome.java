d85acb35f58aad768185ceab878273d521be284b
minseo-00 <limminsoe08@gmail.com>
2025-05-26 17:38:08 +0900

    feat: 사용자 이름 및 연락처 입력 기능 추가

--- Welcome.java
+++ Welcome.java
@@ -1,23 +1,20 @@
 package MainMenu;
 
+import java.util.Scanner;
+
 public class Welcome {
 	public static void main(String[] args) {
-	
-		//	System.out.println("Welcome to Shopping Mall");
-		//	System.out.println("Welcome to Book Market!");
-		
-		String greeting = "Welcome to Shopping Mall";
-		String tagline = "Welcome to Book Market!";
-		
-		System.out.println("***************************************");
-		System.out.println("\t"+greeting);
-		System.out.println("\t"+tagline);
-		System.out.println("***************************************");
-		System.out.println("1. 고객 정보 확인하기 \t4. 바구니에 항목 추가하기");
-		System.out.println("2. 장바구니 상품 목록 보기 \t5. 장바구니의 항목 수량 줄이기");
-		System.out.println("3. 장바구니 비우기 \t6. 장바구니의 항목 삭제하기");
-		System.out.println("7. 영수증 표시하기 \t8. 종료");
-		System.out.println("***************************************");
+		Scanner input = new Scanner(System.in);
 
+        System.out.print("당신의 이름을 입력하세요 : ");
+        String userName = input.next();
+
+        System.out.print("연락처를 입력하세요 : ");
+        int userMobile = input.nextInt();
+
+        String greeting = "Welcome to Shopping Mall";
+        String tagline = "Welcome to Book Market!";
+		
 	}
 }
+
