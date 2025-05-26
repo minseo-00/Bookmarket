bb752b11b2698f1ed58e4bc81cb27ee5cf32ce4b
minseo-00 <limminsoe08@gmail.com>
2025-05-26 17:03:17 +0900

    refactor: 인사말을 greeting, tagline 변수로 분리

--- Welcome.java
+++ Welcome.java
@@ -3,8 +3,13 @@ package MainMenu;
 public class Welcome {
 	public static void main(String[] args) {
 	
-	System.out.println("Welcome to Shopping Mall");
-	System.out.println("Welcome to Book Market!");
-
+		//	System.out.println("Welcome to Shopping Mall");
+		//	System.out.println("Welcome to Book Market!");
+		
+		String greeting = "Welcome to Shopping Mall";
+		String tagline = "Welcome to Book Market!";
+		
+		System.out.println("\t"+greeting);
+		System.out.println("\t"+tagline);
 	}
 }
